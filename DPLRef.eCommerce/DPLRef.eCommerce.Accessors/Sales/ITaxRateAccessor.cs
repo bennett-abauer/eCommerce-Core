b0eacc8ab387e472f1fdd5c87020fa3272407663
bennett-abauer <61594943+bennett-abauer@users.noreply.github.com>
2020-06-22 17:28:03 -0500

    added psuedo proper tax calculation engin/API

---
+++ DPLRef.eCommerce/DPLRef.eCommerce.Accessors/Sales/ITaxRateAccessor.cs
@@ -0,0 +1,13 @@
+﻿using DPLRef.eCommerce.Common.Contracts;
+using DPLRef.eCommerce.Common.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DPLRef.eCommerce.Accessors.Sales
+{
+    public interface ITaxRateAccessor : IServiceContractBase
+    {
+        decimal Rate(Address address);
+    }
+}
---
+++ DPLRef.eCommerce/DPLRef.eCommerce.Tests.AccessorTests/TaxRateAccessorTests.cs
@@ -0,0 +1,34 @@
+﻿using DPLRef.eCommerce.Accessors;
+using DPLRef.eCommerce.Accessors.Sales;
+using DPLRef.eCommerce.Common.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DPLRef.eCommerce.Tests.AccessorTests
+{
+    [TestClass]
+    public class TaxAccessorTests
+    {
+        private ITaxRateAccessor CreateAccessor()
+        {
+            var context = new Common.Contracts.AmbientContext();
+            var factory = new AccessorFactory(
+                context,
+                new Utilities.UtilityFactory(context));
+            return factory.CreateAccessor<ITaxRateAccessor>();
+        }
+
+        [TestMethod]
+        public void TaxAccessor_LincolnNe()
+        {
+            var accessor = CreateAccessor();
+            var lincoln = new Address()
+            {
+                Postal = "68512"
+            };
+            accessor.Rate(lincoln);
+        }
+    }
+}
